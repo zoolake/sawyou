b672b13909a6b7c0607afe34cb1d3ff572971c72
sw200662 <sw200662@naver.com>
2022-03-18 16:58:35 +0900

    Feat : 로그인 페이지 임시 작성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signup from './pages/User/SignupPage';
+import Login from './pages/User/LoginPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/accounts/signup" element={<Signup/>} />
+        <Route path="/accounts/Login" element={<Login/>} />
       </Routes>
 
     </BrowserRouter>
--- frontend/src/components/Auth/Login/index.jsx
+++ frontend/src/components/Auth/Login/index.jsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Wrapper from './styles';
+import TextField from '@mui/material/TextField';
 
 const Login = (props) => {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
 
+  return (
+    <Wrapper>
+    <div>
+      <Grid container sx={{ height: '100vh' }}>
+
+      <Box component="form" noValidate sx={{ mt: 1 }}>
+                
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="Id"
+                  label="아이디"
+                  name="Id"
+                  autoComplete="Id"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="비밀번호"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  로그인
+                </Button>
+                
+              </Box>
+        </Grid>
+
+    </div>
+    </Wrapper>
+    
+
+  )
 };
 
 export default Login;
