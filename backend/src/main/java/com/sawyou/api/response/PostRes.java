b9e8f27d7479f20e75fc2908e06c11c9ebb573bf
박건우 <hggygw77@naver.com>
2022-03-23 00:22:51 +0900

    Fix : [BE] .gitignore application.properties 경로 변경

---
+++ backend/src/main/java/com/sawyou/api/response/PostRes.java
@@ -0,0 +1,31 @@
+package com.sawyou.api.response;
+
+import com.sawyou.db.entity.User;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+/**
+ * 회원 본인 정보 조회 API ([GET] /api/v1/users/me) 요청에 대한 응답값 정의.
+ */
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+@ApiModel("UserResponse")
+public class UserRes {
+    @ApiModelProperty(name = "User ID")
+    private String userId;
+    @ApiModelProperty(name = "User Name")
+    private String userName;
+    @ApiModelProperty(name = "User Email")
+    private String userEmail;
+    @ApiModelProperty(name = "User Desc")
+    private String userDesc;
+    @ApiModelProperty(name = "User Profile")
+    private String userProfile;
+
+    @ApiModelProperty(name = "팔로잉 여부")
+    private boolean isFollowing;
+}
