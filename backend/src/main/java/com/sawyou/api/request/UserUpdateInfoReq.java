8506ebde2da1360bd73729dc45560827674a1321
yeon97 <cjcm1525@naver.com>
2022-03-22 13:39:14 +0900

    Feat : [BE] 비밀번호 수정 기능 추가

--- backend/src/main/java/com/sawyou/api/request/UserUpdateInfoReq.java
+++ backend/src/main/java/com/sawyou/api/request/UserUpdateInfoReq.java
@@ -1,5 +1,7 @@
 package com.sawyou.api.request;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,10 +10,16 @@ import lombok.NoArgsConstructor;
 @Getter
 @Builder
 @NoArgsConstructor @AllArgsConstructor
+@ApiModel("UserUpdateInfoRequest")
 public class UserUpdateInfoReq {
+    @ApiModelProperty(name = "유저 ID", example = "your_new_id")
     private String userId;
+    @ApiModelProperty(name = "유저 이름/닉네임", example = "your_new_name")
     private String userName;
+    @ApiModelProperty(name = "유저 이메일", example = "your_new_email")
     private String userEmail;
+    @ApiModelProperty(name = "유저 자기소개", example = "your_new_desc")
     private String userDesc;
+    @ApiModelProperty(name = "유저 프로필 사진", example = "your_new_profile")
     private String userProfile;
 }
---
+++ backend/src/main/java/com/sawyou/api/request/UserUpdatePwdReq.java
@@ -0,0 +1,15 @@
+package com.sawyou.api.request;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor @AllArgsConstructor
+@ApiModel("UserUpdatePwdRequest")
+public class UserUpdatePwdReq {
+    @ApiModelProperty(name = "유저 비밀번호", example = "your_new_password")
+    private String userPwd;
+}
