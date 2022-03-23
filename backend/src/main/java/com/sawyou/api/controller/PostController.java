149868c2ca6d426614c99b83996f99a1670d2f4f
박건우 <hggygw77@naver.com>
2022-03-23 13:51:39 +0900

    Fix : [BE] 게시글 작성 API builder 패턴 적용

--- backend/src/main/java/com/sawyou/api/controller/PostController.java
+++ backend/src/main/java/com/sawyou/api/controller/PostController.java
@@ -3,10 +3,11 @@ package com.sawyou.api.controller;
 import com.sawyou.api.request.PostWriteReq;
 import com.sawyou.api.service.PostService;
 import com.sawyou.common.auth.SawyouUserDetails;
-import com.sawyou.common.model.response.BaseResponseBody;
 import com.sawyou.db.entity.Post;
-import com.sawyou.db.entity.User;
 import io.swagger.annotations.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -32,9 +33,9 @@ public class PostController {
 			@ApiResponse(code = 409, message = "게시글 작성 실패"),
 			@ApiResponse(code = 500, message = "서버 오류")
 	})
-	public ResponseEntity<? extends BaseResponseBody> writePost(@ApiIgnore Authentication authentication, @RequestBody @ApiParam(value = "게시글 작성 데이터", required = true) PostWriteReq postWrite) {
+	public ResponseEntity<Result> writePost(@ApiIgnore Authentication authentication, @RequestBody @ApiParam(value = "게시글 작성 데이터", required = true) PostWriteReq postWrite) {
 		// 인증 토큰 확인, 올바르지 않은 토큰일 경우에도 401 자동 리턴
-		if (authentication == null) return ResponseEntity.status(401).body(BaseResponseBody.of(401, "인증 실패"));
+		if (authentication == null) return ResponseEntity.status(401).body(Result.builder().status(401).message("인증 실패").build());
 
 		// 토큰에서 사용자의 userSeq 값 추출
 		SawyouUserDetails userDetails = (SawyouUserDetails) authentication.getDetails();
@@ -43,8 +44,16 @@ public class PostController {
 		Post post = postService.writePost(postWrite.getPostContent(), userSeq);
 
 		// 게시글이 제대로 작성되지 않았을 경우
-		if (post == null) return ResponseEntity.status(401).body(BaseResponseBody.of(409, "게시글 작성 실패"));
-		return ResponseEntity.status(401).body(BaseResponseBody.of(201, "게시글 작성 성공"));
+		if (post == null) return ResponseEntity.status(409).body(Result.builder().status(409).message("게시글 작성 실패").build());
+		return ResponseEntity.status(201).body(Result.builder().status(201).message("게시글 작성 성공").build());
 	}
 
+	@Data
+	@AllArgsConstructor
+	@Builder
+	static class Result<T> {
+		private T data;
+		private int status;
+		private String message;
+	}
 }
