e0e5ec443fbe74dbd790abfe5a86c606ed4baf2e
sw200662 <sw200662@naver.com>
2022-03-30 14:50:16 +0900

    Feat : [FE] Nft modal 추가 및 프로필 3가지 리스트 추가

---
+++ frontend/src/components/Profile/ListModal/styles.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  .img2 {
+    width: 100%;
+    height: 100%;
+  }
+  .div2 {
+    height: 100%;
+  }
+
+`;
+
+export default Wrapper;
