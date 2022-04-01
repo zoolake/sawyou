9e93011ef337c543e6c6a71b3f0c45f39451e726
sw200662 <sw200662@naver.com>
2022-04-01 14:11:06 +0900

    Fix : [FE] 로그인 오류 해결

--- frontend/src/api/index.jsx
+++ frontend/src/api/index.jsx
@@ -3,11 +3,17 @@ const Token = localStorage.getItem('access_token');
 
 const API_BASE_URL = 'https://sawyou.kro.kr/api/v1/'
 
+let a = null
+
+if (Token){
+  a = `Bearer ${Token}`
+}
+
 function CreateInstance() {
   const instance = axios.create({
     baseURL: API_BASE_URL,
     headers: {
-      "Authorization" : `Bearer ${Token}`
+      "Authorization" : `${a}`
     }
   });
   return instance;
