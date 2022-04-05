076b178195817d2f4308c97887bcb9e9bc3f7dce
sw200662 <sw200662@naver.com>
2022-04-05 10:08:50 +0900

    Fix : [FE] listmodal props 잘못되던거 수정

--- frontend/src/components/Profile/ListModal/index.jsx
+++ frontend/src/components/Profile/ListModal/index.jsx
@@ -217,7 +217,7 @@ const Postmodal = ({ item }) => {
             <Button onClick={handleChange} sx={{ width: '5%', minHeight: 0, minWidth: 40 }}><AutoFixNormalIcon sx={{ color: 'black' }}></AutoFixNormalIcon></Button>
             <Button onClick={onSendDelete} sx={{ width: '5%', minWidth: 40 }}><DeleteIcon sx={{ color: 'black' }}></DeleteIcon></Button>
           </Box>
-          <Box sx={{ height: '90%' }}>{item.postContent}</Box>
+          <Box sx={{ height: '90%' }}>{post.postContent}</Box>
           {
             isMintingLoaded === true ? <Box sx={{ textAlign: 'center' }}><CircularProgress /></Box> :
               <div>
