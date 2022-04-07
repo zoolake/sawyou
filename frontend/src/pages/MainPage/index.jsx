ad19d3c557db9fa0b0fd1d2b98d4e14b46c076bd
sw200662 <sw200662@naver.com>
2022-04-08 05:42:23 +0900

    Fix : [FE] 메인페이지

--- frontend/src/pages/MainPage/index.jsx
+++ frontend/src/pages/MainPage/index.jsx
@@ -9,6 +9,7 @@ const MainPage = (props) => {
   const [post,setPost] = useState('')
   const [loading,setLoading] = useState(true)
 
+
   const Read = async () => {
     const res = await ReadFollowingPost().then((res) => setPost(res.data.data), setLoading(false))
 
@@ -20,14 +21,9 @@ const MainPage = (props) => {
 
   return (
     <Wrapper>
-      { loading ? null : (
-        post ? post.map((data) => (
-          <Main key={data.postSeq} data={data}></Main>
-        )) : <Box sx={{display:'flex', justifyContent:'center', mt:10}}><img style={{position:'relative', margin: 'auto'}} src="images/main.png"></img></Box>
-      )}
-      {/* {post ? post.map((data) => (
+      {post ? post.map((data) => (
         <Main key={data.postSeq} data={data}></Main>
-      )): <Box sx={{display:'flex', justifyContent:'center', mt:10}}><img style={{position:'relative', margin: 'auto'}} src="images/main.png"></img></Box>}        */}
+      )): <Box sx={{display:'flex', justifyContent:'center', mt:10}}><img style={{position:'relative', margin: 'auto'}} src="images/main.png"></img></Box>}       
     </Wrapper>
   )
 
